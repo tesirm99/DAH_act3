6a7c850a76fa8703c91eefc6e0b665a979a3917f
Tesi <tesiroblesmarin@gmail.com>
2023-03-16 16:26:53 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nativeops',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
